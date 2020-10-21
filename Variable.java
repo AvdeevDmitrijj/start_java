aee0c742e9604b279d7d1eff69c5bbf1d776ad26
Avdeev Dmitrijj <avdeevdmitrijj@gmail.com>
2020-10-22 01:39:58 +0300

    first homework

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+
+  public static void main(String[] args) {
+    byte a = 1;
+    short b = 64;
+    int c = 240;
+    float d = 6;
+    long e = 4;
+    double f = 2.3;
+    char g = 'W';
+    boolean j = true;
+    System.out.println("Number of computers " + a);
+    System.out.println("System Type " + b);
+    System.out.println("Hard Memory " + c);
+    System.out.println("RAM " + d);
+    System.out.println("Amount of cores " + e);
+    System.out.println("Core Frequncy " + f);
+    System.out.println("OS " + g);
+    System.out.println("Study " + j);
+  }
+}
